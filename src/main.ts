e7161e08b20a794943d95e4506f0973530f23fb9
Sky-walkerX <khandelwalnaman0101@gmail.com>
2025-09-19 10:32:33 +0530

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  // Enable CORS for frontend integration
+
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3333', 'https://fac-frontend.vercel.app/'],
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3333',
+      'https://fac-frontend.vercel.app',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Accept, Authorization, X-Requested-With',
     credentials: true,
   });
 
-  await app.listen(3001);
-  console.log('Faculty Portfolio API is running on http://localhost:3001');
+  await app.listen(process.env.PORT || 3001);
+  console.log('Faculty Portfolio API is running');
 }
 
 bootstrap();
